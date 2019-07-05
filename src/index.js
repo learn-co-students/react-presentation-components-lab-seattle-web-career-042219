6a9aafe4972640e0a954af9f8e5fe91d1aa3d0b1
Milan <milan.dhar@gmail.com>
2019-07-05 15:16:42 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+const handleClick = (color) => {
+  console.log(color)
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
   document.getElementById('root')
 );
